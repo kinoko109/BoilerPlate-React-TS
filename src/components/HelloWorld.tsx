567b9776ca2536304220cb382449b3ae48dbc838
松岡龍紀 <tatsuaki_matsuoka@bita.jp>
2020-10-11 12:29:33 +0900

    add: ReactとTSのボイラープレート追加

---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type HelloWorldProps = {
+  text: string;
+};
+
+export const HelloWorld: React.FC<HelloWorldProps> = (props) => {
+  const { text } = props;
+  return (
+    <p>
+      {text}
+      Hello World
+    </p>
+  );
+};
