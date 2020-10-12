3f27e48b5cab09e1b0096aef4cd79e9a441eb1c5
松岡龍紀 <tatsuaki_matsuoka@bita.jp>
2020-10-12 21:21:37 +0900

    update: webpack.config修正、@webpack-dev-server追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,21 @@
 module.exports = {
-  // モード値を production に設定すると最適化された状態で、
-  // development に設定するとソースマップ有効でJSファイルが出力される
+  // ソースマップ有効
   mode: "development",
-
-  // メインとなるJavaScriptファイル（エントリーポイント）
+  // エントリーポイント
   entry: "./src/pages/index.tsx",
-  // ファイルの出力設定
+  // 出力設定
   output: {
-    //  出力ファイルのディレクトリ名
+    //  ディレクトリ名
     path: `${__dirname}/dist`,
-    // 出力ファイル名
+    // ァイル名
     filename: "main.js"
   },
   module: {
     rules: [
       {
-        // 拡張子 .ts もしくは .tsx の場合
+        // .ts, .tsx
         test: /\.tsx?$/,
-        // TypeScript をコンパイルする
+        // TypeScriptコンパイル
         use: "ts-loader"
       }
     ]
@@ -25,5 +23,9 @@ module.exports = {
   // import 文で .ts や .tsx ファイルを解決するため
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  devServer: {
+    contentBase: "dist",
+    open: true,
   }
 };
